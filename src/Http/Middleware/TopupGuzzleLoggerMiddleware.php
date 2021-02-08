eb3fa665ca0b986fd16e0dcce90d40e5d28b6630
MD AL AMIN <m3alamin@gmail.com>
2021-02-08 15:39:37 +0600

    bux fix | improvement | changed guzzle middleware

--- src/Http/Middleware/TopupGuzzleLoggerMiddleware.php
+++ src/Http/Middleware/TopupGuzzleLoggerMiddleware.php
@@ -2,13 +2,21 @@
 
 namespace Topup\Logger\Http\Middleware;
 
+use Exception;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Request;
 use Topup\Logger\Models\Logger;
 
 class TopupGuzzleLoggerMiddleware
 {
+    protected $_log = [];
 
+    /**
+     * Guzzle handler to log request & response
+     *
+     * @return \Closure
+     */
     public function log()
     {
         return function (callable $handler) {
@@ -16,58 +24,52 @@ class TopupGuzzleLoggerMiddleware
                 RequestInterface $request,
                 array $options
             ) use ($handler) {
-                $reqHeader = json_encode($request->getHeaders());
-                $reqBody = (string)$request->getBody();
-                $reqUrl = $request->getUri();
-                $reqMethod = $request->getMethod();
+
                 $req = [
-                    'header' => $reqHeader,
-                    'body' => $reqBody,
-                    'url' => $reqUrl,
-                    'method' => $reqMethod,
+                    'header' => $request->getHeaders(),
+                    'body' => $request->getBody(),
+                    'url' => $request->getUri(),
+                    'method' => $request->getMethod(),
                 ];
+
+                $this->_log['req'] = $req;
+
                 $promise = $handler($request, $options);
                 return $promise->then(
-                    function (ResponseInterface $response) use ($req) {
-                        $resBody = $response->getBody();
-                        logger(gettype($resBody));
-                        $resHeader = json_encode($response->getHeaders());
-
-                        $log = [
-                            'req' => $req,
-                            'res' => [
-                                'header' => $resHeader,
-                                'body' => $resBody,
-                            ],
+                    function (ResponseInterface $response) {
+                        $res = [
+                            'header' => $response->getHeaders(),
+                            'body' => $response->getBody(),
                         ];
+                        $this->_log['res'] = $res;
 
-                        $this->_save($log);
+                        $this->_save();
 
-                        return $response->withHeader('x-test', 123);
+                        return $response;
                     }
                 );
             };
         };
     }
 
-    protected function _save($log)
+    protected function _save()
     {
         $logger = new Logger();
         $logger->user_id = auth()->id() ?? null;
-        $logger->method = $log['req']['method'];
+        $logger->method = $this->_log['req']['method'];
         $logger->type = 'outbound';
-        $logger->url = $log['req']['url'];
-        $logger->request_header = $log['req']['header'];
-        $logger->response_header = $log['res']['header'];
-        $logger->request_body = $log['req']['body'];
-        $logger->response_body = $log['res']['body'];
-        $logger->ip = \Request::ip();
+        $logger->url = $this->_log['req']['url'];
+        $logger->request_header = json_encode($this->_log['req']['header']);
+        $logger->response_header = json_encode($this->_log['res']['header']);
+        $logger->request_body = (string)$this->_log['req']['body'];
+        $logger->response_body = (string)$this->_log['res']['body'];
+        $logger->ip = Request::ip();
         $logger->duration = number_format(microtime(true) - LARAVEL_START, 3);
         $logger->request_time = now();
 
         try {
             $logger->save();
-        } catch (\Exception $exception) {
+        } catch (Exception $exception) {
             logger()->error('Could not save API log');
             logger()->error($exception);
         }
